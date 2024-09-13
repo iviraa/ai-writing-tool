3e8d657126b3fd19deffb1f93e3ba7a640962ce4
iviraa <chetanchal.cs@gmail.com>
2024-09-13 06:01:35 -0500

    privy configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css';
+
+import {PrivyProvider} from '@privy-io/react-auth';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <PrivyProvider
+      appId="cm10lmh7603cl93b9x6nfzriv"
+      config={{
+        // Display email and wallet as login methods
+        loginMethods: ['email', 'google','github'],
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
+      <App />
+    </PrivyProvider>
+  </React.StrictMode>,
+);
