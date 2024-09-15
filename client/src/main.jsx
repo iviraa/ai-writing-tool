631cc1f65092d429566c7b6cb2ca700e848f0cbb
iviraa <chetanchal.cs@gmail.com>
2024-09-15 05:16:09 -0500

    removed logo in login screen

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
+import "./index.css";
 
-import {PrivyProvider} from '@privy-io/react-auth';
+import { PrivyProvider } from "@privy-io/react-auth";
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
@@ -15,20 +15,19 @@ root.render(
       appId="cm10lmh7603cl93b9x6nfzriv"
       config={{
         // Display email and wallet as login methods
-        loginMethods: ['email', 'google','github'],
+        loginMethods: ["email", "google", "github"],
         // Customize Privy's appearance in your app
         appearance: {
-          theme: 'light',
-          accentColor: '#676FFF',
-          logo: 'https://your-logo-url',
+          theme: "light",
+          accentColor: "#676FFF",
         },
         // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
-          createOnLogin: 'users-without-wallets',
+          createOnLogin: "users-without-wallets",
         },
       }}
     >
       <App />
     </PrivyProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
