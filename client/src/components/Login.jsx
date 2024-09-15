c0072752a7bea71c400bfdb1fbbb629b5cb0ccd7
iviraa <chetanchal.cs@gmail.com>
2024-09-15 04:59:08 -0500

    comments

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,23 +22,18 @@ function LoginPage() {
 
   return (
     <div className="bg-gray-100 text-gray-900 font-sans min-h-screen flex items-center justify-center">
-      {/* Login Section */}
       <section className="bg-gradient-to-r from-blue-300 to-green-300 text-center py-20 shadow-lg rounded-lg p-8 max-w-md w-full">
         <h2 className="text-4xl font-extrabold text-center text-white mb-6">
           Welcome to Inkling
         </h2>
 
-        {/* Button Section */}
         <div className="space-y-4">
-          {/* Login Button */}
           <button
             onClick={login}
             className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-400 transition duration-300"
           >
             Login
           </button>
-
-          {/* Get Started Button */}
 
           <button
             onClick={login}
